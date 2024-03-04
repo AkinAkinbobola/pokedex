e94d77bc5f0e8ca72f0cc4270c5d8e92f440a7ff
Akin Akinbobola <akinbobola71@gmail.com>
2024-03-04 13:22:34 +0100

    Change image path

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,7 @@
 <template>
   <nav class="p-10 container mx-auto">
     <NuxtLink class="font-honk text-6xl cursor-pointer" to="/">
-      <img src="/public/icons/Pokedex-logo.svg"/>
+      <img src="/icons/Pokedex-logo.svg"/>
     </NuxtLink>
   </nav>
 </template>
