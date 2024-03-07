32464fc3d27222299b8a0e3818fd18866aa755d7
Akin Akinbobola <akinbobola71@gmail.com>
2024-03-07 08:15:56 +0100

    Add Loading.vue screen

---
+++ components/Loading.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+  <div class="flex justify-center items-center mt-20">
+    <div class="animate-pulse">
+      <img src="/icons/large-height.svg" alt="" class="h-56">
+    </div>
+  </div>
+</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 const offset = ref(0)
 const limit = ref(50)
+const loading = ref(true)
 
 const {data: pokemons, pending, error} = await useFetch(() => `/api/pokemon/all`, {
   query: {
@@ -11,22 +12,29 @@ const {data: pokemons, pending, error} = await useFetch(() => `/api/pokemon/all`
 const handleNext = () => {
   offset.value += 30
 }
+onMounted(() => {
+  setTimeout(() => loading.value = false, 200)
+})
 </script>
 
 <template>
   <div>
-    <div v-if="error" class="text-red-400 text-center font-bold text-2xl">
-      Oops an error occurred. Please try again later
+    <div v-if="loading">
+      <Loading/>
     </div>
     <div v-else>
-      <div v-if="pending">
-        <PokemonSkeletons/>
+      <div v-if="error" class="text-red-400 text-center font-bold text-2xl">
+        Oops an error occurred. Please try again later
       </div>
       <div v-else>
-        <PokemonCards :pokemons="pokemons.pokemonData" :count="pokemons.count"/>
-        <button @click="handleNext">Next</button>
+        <div v-if="pending">
+          <PokemonSkeletons/>
+        </div>
+        <div v-else>
+          <PokemonCards :pokemons="pokemons.pokemonData" :count="pokemons.count"/>
+        </div>
       </div>
     </div>
-
   </div>
+
 </template>
