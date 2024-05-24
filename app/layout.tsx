c81ae26a9761c156cbc38da009ff7a866bd5e385
Akin Akinbobola <akinbobola71@gmail.com>
2024-05-24 09:54:09 +0100

    Add colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import {Roboto} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
-const inter = Inter({subsets: ["latin"]});
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "500", "700", "900"]
+});
 
 export const metadata: Metadata = {
     title: "Pokedex",
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body className={roboto.className}>
         <div className={"max-w-7xl mx-auto pt-12"}>
             <Header/>
             {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,66 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                // Site colors
+                blue: "#C9D2EA",
+                darkBlue: "#416EDF",
+                yellow: "#FFCE31",
+                darkGray: "#212121",
+                lightGray: "#F5F7FB",
+
+                // Pokemon colors
+                fire: "#FF6464",
+                bug: "#C9FF84",
+                water: "#9FF3FF",
+                poison: "#D89CFD",
+                normal: "#CBCBCB",
+                rock: "#CFC06F",
+                ground: "#FFBF72",
+                fighting: "#FF699F",
+                ghost: "#B592FF",
+                psychic: "#FF5E60",
+                ice: "#AEFFF4",
+                dragon: "#87C5FF",
+                dark: "#8F8F8F",
+                fairy: "#FFA2E3",
+                electric: "#FFFA86",
+                steel: "#A4FFE9",
+                grass: "#80E177",
+
+                //  Pokemon Background
+                fireBackground: "#EB6C6C",
+                bugBackground: "#91AC22",
+                waterBackground: "#009ACB",
+                poisonBackground: "#7E00CB",
+                normalBackground: "#B6B6B6",
+                rockBackground: "#857D57",
+                groundBackground: "#A77437",
+                fightingBackground: "#BA114E",
+                ghostBackground: "#6B2BF1",
+                psychicBackground: "#C4484A",
+                iceBackground: "#3A9D90",
+                dragonBackground: "#1268B8",
+                darkBackground: "#373737",
+                fairyBackground: "#C01A8D",
+                electricBackground: "#B7B117",
+                steelBackground: "#448F85",
+                grassBackground: "#1EBA11",
+
+            },
+            backgroundImage: {
+                flying: "linear-gradient(to right, #CBE9FF, #2299EE)",
+                flyingBackground: "linear-gradient(to right, #CBE9FF, #2299EE)"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
