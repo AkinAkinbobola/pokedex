5462408c5ea05750281d27b0b54040d64d0fde2b
Akin Akinbobola <akinbobola71@gmail.com>
2024-08-29 15:31:35 +0100

    Add search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import React from "react";
 import { Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const query = (form.query as HTMLInputElement).value.trim();
+
+    if (!query) return;
+
+    router.push(`?q=${encodeURIComponent(query)}`);
+  };
+
   return (
-    <div
+    <form
+      onSubmit={handleSearch}
       className={
         "my-[40px] flex justify-between gap-2 p-2 shadow-sm bg-white rounded-2xl"
       }
@@ -16,16 +31,18 @@ const SearchBar = () => {
         <input
           placeholder={"Pokemon name, number or type"}
           className={"ps-12 placeholder-darkBlue h-full w-full outline-none"}
+          name={"query"}
         />
       </div>
       <Button
+        type={"submit"}
         className={
           "bg-yellow text-darkBlue hover:bg-yellow/80 hover:text-darkBlue/80 px-[50px] py-[12px]"
         }
       >
         Search
       </Button>
-    </div>
+    </form>
   );
 };
 
