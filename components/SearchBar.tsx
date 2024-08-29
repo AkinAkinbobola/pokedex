5afeff0723c0b10ab08128dc0d334c57ad7a1ee3
Akin Akinbobola <akinbobola71@gmail.com>
2024-08-29 15:21:22 +0100

    Fix search bar again

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const SearchBar = () => {
   return (
-    <div className={"relative my-[40px]"}>
-      <Search
-        className={"absolute top-1/2 left-3 -translate-y-1/2 text-darkBlue"}
-      />
-      <Input
-        placeholder={"Pokemon name, number or type"}
-        className={"pl-14 placeholder-darkBlue"}
-      />
-
+    <div
+      className={
+        "my-[40px] flex justify-between gap-2 p-2 shadow-sm bg-white rounded-2xl"
+      }
+    >
+      <div className={"relative flex-1"}>
+        <Search
+          className={"absolute top-1/2 left-2 -translate-y-1/2 text-darkBlue"}
+        />
+        <input
+          placeholder={"Pokemon name, number or type"}
+          className={"ps-12 placeholder-darkBlue h-full w-full outline-none"}
+        />
+      </div>
       <Button
         className={
-          "absolute right-0 top-1/2 -translate-y-1/2 bg-yellow text-darkBlue hover:bg-yellow/80 hover:text-darkBlue/80 px-[50px] py-[12px]"
+          "bg-yellow text-darkBlue hover:bg-yellow/80 hover:text-darkBlue/80 px-[50px] py-[12px]"
         }
       >
         Search
