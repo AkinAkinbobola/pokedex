060b3746fa6e1a98502f90a11f2123d6a3d90990
Akin Akinbobola <akinbobola71@gmail.com>
2024-08-30 20:56:26 +0100

    Add infinite query

--- app/api/pokemons/route.ts
+++ app/api/pokemons/route.ts
@@ -1,15 +1,34 @@
 import ky from "ky";
-import { GetAllPokemonResponse } from "@/lib/types";
+import { GetAllPokemonResponse, PokemonData, PokemonsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
 
-export const GET = async () => {
-  const pokemonResponse = await ky
-    .get("https://pokeapi.co/api/v2/pokemon")
-    .json<GetAllPokemonResponse>();
+export const GET = async (req: NextRequest) => {
+  const offset = Number(req.nextUrl.searchParams.get("offset")) || 0;
+  const limit = 10;
+  try {
+    const pokemonResponse = await ky
+      .get("https://pokeapi.co/api/v2/pokemon", {
+        searchParams: {
+          limit,
+          offset,
+        },
+      })
+      .json<GetAllPokemonResponse>();
 
-  const pokemonPromises = pokemonResponse.results.map(async (pokemon) => {
-    return ky.get(pokemon.url).json();
-  });
+    const pokemonPromises = pokemonResponse.results.map(async (pokemon) => {
+      return ky.get(pokemon.url).json<PokemonData>();
+    });
 
-  const detailedPokemon = await Promise.all(pokemonPromises);
-  return Response.json(detailedPokemon);
+    const detailedPokemon = await Promise.all(pokemonPromises);
+    const nextOffset =
+      pokemonResponse.results.length === limit ? offset + limit : null;
+
+    const data: PokemonsPage = {
+      pokemons: detailedPokemon,
+      nextOffset,
+    };
+    return Response.json(data);
+  } catch (e) {
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 };
--- components/Pokemons.tsx
+++ components/Pokemons.tsx
@@ -1,28 +1,43 @@
 "use client";
 
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ky from "ky";
-import { PokemonData } from "@/lib/types";
+import { PokemonsPage } from "@/lib/types";
 import Pokemon from "@/components/Pokemon";
 import PokemonsLoadingSkeleton from "@/components/PokemonsLoadingSkeleton";
+import { useInView } from "react-intersection-observer";
 
 interface PokemonsProps {
   query: string;
 }
 
 const Pokemons = ({ query }: PokemonsProps) => {
-  const { isLoading, data, error } = useQuery({
-    queryKey: ["pokemons", query],
-    queryFn: () =>
-      ky
-        .get("/api/pokemons", {
-          searchParams: {
-            q: query,
-          },
-        })
-        .json<PokemonData[]>(),
+  const { isLoading, data, error, fetchNextPage, hasNextPage, isFetching } =
+    useInfiniteQuery({
+      queryKey: ["pokemons", query],
+      queryFn: ({ pageParam }) =>
+        ky
+          .get("/api/pokemons", {
+            searchParams: {
+              q: query,
+              offset: pageParam,
+            },
+          })
+          .json<PokemonsPage>(),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => lastPage.nextOffset,
+    });
+
+  const { ref } = useInView({
+    // rootMargin: "200px",
+    onChange(inView) {
+      if (inView && hasNextPage && !isFetching) {
+        fetchNextPage();
+      }
+    },
   });
+  const pokemons = data?.pages.flatMap((page) => page.pokemons) || [];
 
   if (isLoading) {
     return <PokemonsLoadingSkeleton />;
@@ -36,7 +51,7 @@ const Pokemons = ({ query }: PokemonsProps) => {
     );
   }
 
-  if (!data || data.length === 0) {
+  if (!pokemons || pokemons.length === 0) {
     return (
       <div className={"font-bold text-center mt-[102px]"}>No pokemon found</div>
     );
@@ -45,12 +60,13 @@ const Pokemons = ({ query }: PokemonsProps) => {
   return (
     <div
       className={
-        "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-[82px] mt-[102px]"
+        "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-[112px] mt-[102px]"
       }
     >
-      {data.map((pokemon) => (
+      {pokemons.map((pokemon) => (
         <Pokemon pokemon={pokemon} key={pokemon.id} />
       ))}
+      <div ref={ref} />
     </div>
   );
 };
